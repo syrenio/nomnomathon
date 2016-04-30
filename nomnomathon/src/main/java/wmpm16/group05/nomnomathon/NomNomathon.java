dfdb6aaa79c8ec8cdb629114abf7b2f0548199d1
Till Weißer <e1527667@student.tuwien.ac.at>
2016-04-30 16:48:33 +0200

    Project initialized with Camel Sample Project.

---
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/NomNomathon.java
@@ -0,0 +1,19 @@
+package wmpm16.group05.nomnomathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * A sample Spring Boot application that starts the Camel routes.
+ */
+@SpringBootApplication
+public class NomNomathon {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(NomNomathon.class, args);
+    }
+
+}
