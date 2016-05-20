3be526b103b14d3f63a6808ee6b52e43f6273498
Martin <afraidoferrors@users.noreply.github.com>
2016-05-20 09:30:51 +0200

    Added profile properties (for mongodb in this case).
    Set spring.profiles.active to a desired profile name and create another
    application-<<profilename>> and override defaults

--- nomnomathon/src/main/java/wmpm16/group05/nomnomathon/NomNomathon.java
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/NomNomathon.java
@@ -1,38 +1,33 @@
 package wmpm16.group05.nomnomathon;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import wmpm16.group05.nomnomathon.models.Customer;
 import wmpm16.group05.nomnomathon.models.CustomerRepository;
-
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class NomNomathon {
+	private static final Logger log = Logger.getLogger(NomNomathon.class);
+	
     private static final String CAMEL_URL_MAPPING = "/api/*";
     private static final String CAMEL_SERVLET_NAME = "CamelServlet";
+    
+    @Value("${mongoDB.host}")
+    private String mongoHost;
 
     @Autowired
     CustomerRepository customerRepository;
-
+    
     /**
      * A main method to start
      */
@@ -60,7 +55,8 @@ public class NomNomathon {
     
     @Bean
     public Mongo mongoDb(){
-    	return new MongoClient();
+    	log.debug("Resolved Adress for MongoDB: " + this.mongoHost);
+    	return new MongoClient(this.mongoHost);
     }
     
 }
