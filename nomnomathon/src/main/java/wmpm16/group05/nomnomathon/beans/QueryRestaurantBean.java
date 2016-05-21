5fafe63c56b1d74c597649fa2479947f2fe4ed82
agnesfroeschl <agnes.froeschl@gmx.at>
2016-05-21 17:37:52 +0200

    add QueryRestaurantBean instead of Processor

--- nomnomathon/src/main/java/wmpm16/group05/nomnomathon/beans/QueryRestaurantBean.java
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/beans/QueryRestaurantBean.java
@@ -1,25 +1,25 @@
-package wmpm16.group05.nomnomathon.processor;
-
+package wmpm16.group05.nomnomathon.beans;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Agnes on 12.05.16.
+ * Created by Agnes on 21.05.16.
  */
 @Component
-public class QueryRestaurantProcessor implements Processor {
+public class QueryRestaurantBean {
 
+    public void process(Exchange exchange){
+        extractRestaurant(exchange);
+    }
 
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        // System.out.println("findAll: " + exchange.getIn().getBody(ArrayList.class));
+    private void extractRestaurant(Exchange exchange) {
+        System.out.println("findAll Body: " + exchange.getIn().getBody(ArrayList.class));
         List<BasicDBObject> result = exchange.getIn().getBody(ArrayList.class);
         System.out.println("results: " + result.size() + " restaurant/s found");
         BasicDBObject object = result.get(0);
@@ -28,4 +28,6 @@ public class QueryRestaurantProcessor implements Processor {
         BasicDBObject firstDish = (BasicDBObject) dishes.get(0);
         System.out.println("first dish: " + firstDish.get("NAME"));
     }
+
+
 }
