fb5efa302d82eaf783b3bf68895be94a24e1e3f9
Bernhard Müller <bernhard.mueller@gmx.at>
2016-05-21 19:26:49 +0200

    added getter and setter for names

--- nomnomathon/src/main/java/wmpm16/group05/nomnomathon/models/Customer.java
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/models/Customer.java
@@ -42,6 +42,30 @@ public class Customer {
 				+ "]";
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getPhoneNumber() {
         return phoneNumber;
     }
