5e860b0796e4ce8202fa4502b566ac1430760635
Martin <afraidoferrors@users.noreply.github.com>
2016-05-31 10:11:50 +0200

    Recreated demo controller and added test method for capacity check.

--- nomnomathon/src/main/java/wmpm16/group05/nomnomathon/controller/TestController.java
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/controller/TestController.java
@@ -6,18 +6,24 @@ import java.util.Optional;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wmpm16.group05.nomnomathon.domain.OrderRequest;
 import wmpm16.group05.nomnomathon.models.Customer;
 import wmpm16.group05.nomnomathon.models.CustomerRepository;
+import wmpm16.group05.nomnomathon.models.OrderInProcess;
+
+
 
 
 @RestController
-public class EnrichOrderController {
-	private Logger log = Logger.getLogger(EnrichOrderController.class);
+public class TestController {
+	private Logger log = Logger.getLogger(TestController.class);
 	
 	@Autowired
 	private CamelContext context;
@@ -25,8 +31,8 @@ public class EnrichOrderController {
 	@Autowired
     CustomerRepository customerRepository;
 
-	@RequestMapping("/demo/EnrichOrder")
-	public OrderRequest startProcess() {
+	@RequestMapping("/test/EnrichOrder")
+	public OrderRequest testEnrichCustomer() {
 		
 		Optional<Customer> opcustomer = customerRepository.findOneByUserNameAndPassword("bernd","nomnom");
 		log.debug("Found customer: " + (opcustomer.isPresent() ? opcustomer.get() : "No customer found - exiting"));
@@ -54,4 +60,33 @@ public class EnrichOrderController {
 		
 	}
 
+	@RequestMapping("/test/testRestaurantStatus")
+	public void testRestaurantStatus() {
+		log.debug("START TestRestaurantStatus");
+		
+		try {
+			context.addRoutes(new RouteBuilder() {
+
+				@Override
+				public void configure() throws Exception {
+					from("direct:testRestaurantStatus").marshal().json(JsonLibrary.Jackson).setHeader("Exchange.HTTP_METHOD", constant("GET")).to("http://localhost:8080/external/restaurants/pizzza/capacity?METHOD=GET");
+					
+				}});
+			log.debug("ROUTE added");
+		} catch (Exception e) {
+			
+			log.error(e);
+		}
+		OrderInProcess order = new OrderInProcess();
+		
+		ProducerTemplate template = context.createProducerTemplate();
+		template.requestBody("direct:testRestaurantStatus", order);
+		
+		
+		log.debug("END TestRestaurantStatus");
+		
+	}
+	
+
+	
 }
--- nomnomathon/src/main/java/wmpm16/group05/nomnomathon/mocked/RestaurantController.java
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/mocked/RestaurantController.java
@@ -1,5 +1,7 @@
 package wmpm16.group05.nomnomathon.mocked;
 
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,16 +14,25 @@ import javax.websocket.server.PathParam;
  * Created by syrenio on 04/05/16.
  */
 @RestController
-@RequestMapping("/external/restaurants")
 public class RestaurantController {
+	
+private final Logger log = Logger.getLogger(RestaurantController.class);
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/external/restaurants/{id}/capacity", method = RequestMethod.GET)
     @ResponseBody
-    public RestaurantCapacityResponse getRestaurant(@PathParam("id") String id){
+    public RestaurantCapacityResponse getRestaurant(@PathVariable String id){
+    	log.debug("Received status request with restaurantid " + id);
         double rnd = Math.random();
         RestaurantCapacityResponse resp = new RestaurantCapacityResponse();
         resp.setCapacityAvailable(rnd > 0.5);
         return resp;
+    }
+    
+    @RequestMapping(value = "/external/restaurants/{id}/order", method = RequestMethod.POST)
+    @ResponseBody
+    public void sendOrder(@PathVariable String id){
+       log.debug("Received order with restaurantid " + id);
+       
     }
 
 }
