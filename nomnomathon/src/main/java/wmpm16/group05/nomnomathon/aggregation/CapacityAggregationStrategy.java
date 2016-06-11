76e0581719df2517b5c261cc10fc22c634f4e7a6
Martin <martin@weik.at>
2016-06-11 14:06:20 +0200

    Enabled logging in CAS.

--- nomnomathon/src/main/java/wmpm16/group05/nomnomathon/aggregation/CapacityAggregationStrategy.java
+++ nomnomathon/src/main/java/wmpm16/group05/nomnomathon/aggregation/CapacityAggregationStrategy.java
@@ -1,21 +1,21 @@
 package wmpm16.group05.nomnomathon.aggregation;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.apache.log4j.Logger;
+
 import wmpm16.group05.nomnomathon.domain.RestaurantCapacityResponse;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class CapacityAggregationStrategy implements AggregationStrategy{
-    @Override
+	private final static Logger log = Logger.getLogger(CapacityAggregationStrategy.class);
+	
+    @SuppressWarnings("unchecked")
+	@Override
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
 
-        //log.debug("Received answers: " + oldExchange + " / " + newExchange + " from " + newExchange.getProperty(Exchange.RECIPIENT_LIST_ENDPOINT, String.class));
-        //log.debug(value);
+        log.debug("Received answers: " + oldExchange + " / " + newExchange + " from " + newExchange.getProperty(Exchange.RECIPIENT_LIST_ENDPOINT, String.class));
 
         RestaurantCapacityResponse newBody = newExchange.getIn().getBody(RestaurantCapacityResponse.class);
         ArrayList<RestaurantCapacityResponse> list = null;
